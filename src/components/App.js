e7b2d4f6623ac558c8fa0e1ee71cea1f2e93b538
清川　裕太郎 <yuu7353@gmail.com>
2020-06-21 16:46:54 +0900

    initial build

---
+++ src/components/App.js
@@ -0,0 +1,134 @@
+import React from "react";
+
+import Form from "./Form";
+import Todo from "./Todo";
+import CheckAll from "./CheckAll";
+import Filter from "./Filter";
+// ----- 追加 -----
+import EditTodo from "./EditTodo";
+
+let currentId = 0;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filter: "all",
+      todos: []
+    };
+  }
+  render() {
+    const { todos, filter } = this.state;
+    const filteredTodos = todos.filter(({ completed }) => {
+      switch (filter) {
+        case "all":
+          return true;
+        case "uncompleted":
+          return !completed;
+        case "completed":
+          return completed;
+        default:
+          return true;
+      }
+    });
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit} />
+        <CheckAll
+          allCompleted={
+            todos.length > 0 && todos.every(({ completed }) => completed)
+          }
+          onChange={this.handleChangeAllCompleted}
+        />
+        <Filter filter={filter} onChange={this.handleChangeFilter} />
+        <ul>
+          {filteredTodos.map(({ id, text, completed, editing }) => (
+            <li key={id}>
+            
+              {editing ? (
+                <EditTodo
+                  id={id}
+                  text={text}
+                  onCancel={this.handleChangeTodoAttribute}
+                  onSubmit={this.handleUpdateTodoText}
+                />
+               ) : (
+                <Todo
+                  id={id}
+                  text={text}
+                  completed={completed}
+                  onChange={this.handleChangeTodoAttribute}
+                  onDelete={this.handleClickDelete}
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+        <button onClick={this.handleClickDeleteCompleted}>
+          完了済みを全て削除
+        </button>
+      </div>
+    );
+  }
+  handleSubmit = text => {
+    // ----- editingを追加 -----
+    const newTodo = {
+      id: currentId,
+      text: text,
+      completed: false,
+      editing: false
+    };
+    const newTodos = [...this.state.todos, newTodo];
+    this.setState({ todos: newTodos });
+    currentId++;
+  };
+
+  handleChangeAllCompleted = completed => {
+    const newTodos = this.state.todos.map(todo => ({
+      ...todo,
+      completed
+    }));
+    this.setState({ todos: newTodos });
+  };
+  handleChangeFilter = filter => {
+    this.setState({ filter });
+  };
+  // ----- key,valueで更新できるよう修正 -----
+  handleChangeTodoAttribute = (id, key, value) => {
+    const newTodos = this.state.todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          [key]: value
+        };
+      }
+      return todo;
+    });
+    this.setState({ todos: newTodos });
+  };
+  handleUpdateTodoText = (id, text) => {
+    const newTodo = this.state.todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          text,
+          editing: false
+        };
+      }
+      return todo;
+    });
+    this.setState({ todos: newTodo });
+  };
+
+  handleClickDelete = id => {
+    const newTodos = this.state.todos.filter(todo => todo.id !== id);
+    this.setState({ todos: newTodos });
+  };
+  handleClickDeleteCompleted = () => {
+    const newTodos = this.state.todos.filter(({ completed }) => !completed);
+    this.setState({ todos: newTodos });
+  };
+}
+
+export default App;
